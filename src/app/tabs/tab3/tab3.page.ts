1d111fc634926419979cf16a324cfc2462ded5cd
Katerin Sanz Celorrio <ksanz@birt.eus>
2023-11-01 22:26:35 +0100

    commit

---
+++ src/app/tabs/tab3/tab3.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ayuda',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  constructor() {}
+
+}
